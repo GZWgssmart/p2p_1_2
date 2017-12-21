b8fdec56cb8f642ca22334bc0596271cc0738780
ChenZhenCZ <2908903432@qq.com>
2017-12-21 20:44:03 +0800

    修改UserMoneyServiceImpl

--- P2Pproj/src/main/java/top/zzh/service/impl/UserMoneyServiceImpl.java
+++ P2Pproj/src/main/java/top/zzh/service/impl/UserMoneyServiceImpl.java
@@ -5,6 +5,10 @@ import top.zzh.dao.UserMoneyDAO;
 import top.zzh.service.AbstractService;
 import top.zzh.service.UserMoneyService;
 
+/**
+ * @author 陈桢
+ * created time 2017-12-21 20:44
+ */
 @Service
 public class UserMoneyServiceImpl extends AbstractService implements UserMoneyService {
 
