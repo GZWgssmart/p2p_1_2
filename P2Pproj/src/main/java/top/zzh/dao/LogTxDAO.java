8aa30f698ae57e77a1f3cb880897c0025ca2efe3
ChenZhenCZ <2908903432@qq.com>
2017-12-22 10:20:12 +0800

    完成了用户资金表的svae,update,分页，按条件分页， 修改了Logtx的date字段为dateTime ,数据log_tx表date字段修改

---
+++ P2Pproj/src/main/java/top/zzh/dao/LogTxDAO.java
@@ -0,0 +1,10 @@
+package top.zzh.dao;
+
+import org.springframework.stereotype.Repository;
+/**
+ * @author 陈桢
+ * created time 2017-12-22 10:10
+ */
+@Repository
+public interface LogTxDAO extends BaseDAO{
+}
