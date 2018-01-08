0c9b2e78a4ff1ebb69f4864c2b244dba20fda436
ZongjieWu <2012293155@qq.com>
2018-01-08 16:00:36 +0800

    修改ticketVo

--- P2Pproj/src/main/java/top/zzh/vo/TicketVo.java
+++ P2Pproj/src/main/java/top/zzh/vo/TicketVo.java
@@ -6,9 +6,47 @@ import java.util.Date;
 public class TicketVo {
     private Integer kid;
     private Byte type;
+    private String tname;//优惠卷名称
+    private String tname3;
+    private String tintro;//优惠卷简介
+    private String tintro3;
     private BigDecimal tkmoney;//优惠券金额
     private BigDecimal tkmoney4;//优惠券金额
     private BigDecimal tkmoney5;//优惠券金额
+
+
+    public String getTname3() {
+        return tname3;
+    }
+
+    public void setTname3(String tname3) {
+        this.tname3 = tname3.trim();
+    }
+
+    public String getTintro3() {
+        return tintro3;
+    }
+
+    public void setTintro3(String tintro3) {
+        this.tintro3 = tintro3.trim();
+    }
+
+
+    public String getTname() {
+        return tname;
+    }
+
+    public void setTname(String tname) {
+        this.tname = tname.trim();
+    }
+
+    public String getTintro() {
+        return tintro;
+    }
+
+    public void setTintro(String tintro) {
+        this.tintro = tintro.trim();
+    }
 
     public BigDecimal getTkmoney5() {
         return tkmoney5;
